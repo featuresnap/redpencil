c0b23c5d9846a703ceba40560cad007f45c55298
Patrick Kelly <patrick.kelly@sherwin.com>
2019-02-21 06:49:10 -0500

    renamed states for brevity / clarity

--- test/RedPencil.Test/Tests.fs
+++ test/RedPencil.Test/Tests.fs
@@ -10,29 +10,29 @@ module RedPencilTest =
 
     [<Fact>]
     let ``Start State, time passes, counts down days till eligible`` () =
-        let currentState = IneligibleForPromotion {DaysUntilEligible = 30; LastPrice = 100M }
+        let currentState = Ineligible {DaysUntilEligible = 30; LastPrice = 100M }
         let transition = DaysElapse(29)
-        let expectedState = IneligibleForPromotion {DaysUntilEligible = 1; LastPrice = 100M }
+        let expectedState = Ineligible {DaysUntilEligible = 1; LastPrice = 100M }
         Assert.Equal(expectedState, currentState |> applyTransition(transition))
        
     [<Fact>]
     let ``Start State, price decrease, restarts countdown with new price`` () =
-        let currentState = IneligibleForPromotion {DaysUntilEligible = 1; LastPrice = 100M }
+        let currentState = Ineligible {DaysUntilEligible = 1; LastPrice = 100M }
         let transition = PriceDecrease(10M)
-        let expectedState = IneligibleForPromotion {DaysUntilEligible = 30; LastPrice = 90M }
+        let expectedState = Ineligible {DaysUntilEligible = 30; LastPrice = 90M }
         Assert.Equal(expectedState, currentState |> applyTransition(transition))
           
     [<Fact>]
     let ``Start State, price increase, restarts countdown with new price`` () =
-        let currentState = IneligibleForPromotion {DaysUntilEligible = 1; LastPrice = 100M }
+        let currentState = Ineligible {DaysUntilEligible = 1; LastPrice = 100M }
         let transition = PriceIncrease(10M)
-        let expectedState = IneligibleForPromotion {DaysUntilEligible = 30; LastPrice = 110M }
+        let expectedState = Ineligible {DaysUntilEligible = 30; LastPrice = 110M }
         Assert.Equal(expectedState, currentState |> applyTransition(transition))
           
     [<Fact>]
-    let ``Start State, 30 days pass, transition to EligibleForPromotion`` () =
-        let currentState = IneligibleForPromotion {DaysUntilEligible = 1; LastPrice = 100M }
+    let ``Start State, 30 days pass, transition to Eligible`` () =
+        let currentState = Ineligible {DaysUntilEligible = 1; LastPrice = 100M }
         let transition = DaysElapse(30)
-        let expectedState = EligibleForPromotion {Lower = 70M; Upper = 95M }
+        let expectedState = Eligible {Lower = 70M; Upper = 95M }
         Assert.Equal(expectedState, currentState |> applyTransition(transition))
        
