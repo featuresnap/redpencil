26560e79219bdf08b67bb36996c0197f477e08d3
Patrick Kelly <pkelly14159@hotmail.com>
2019-03-02 20:42:45 -0500

    friendlier test names

--- test/RedPencil.Test/Tests.fs
+++ test/RedPencil.Test/Tests.fs
@@ -3,34 +3,35 @@ namespace Tests
 open System
 open Xunit
 
-module RedPencilTest = 
+module WhenIneligible = 
 
     open Xunit
     open RedPencil.Library
+    
 
     [<Fact>]
-    let ``Start State, time passes, counts down days till eligible`` () =
+    let ``Passage of time is counted toward days until eligible`` () =
         let currentState = Ineligible {DaysUntilEligible = 30; LastPrice = 100M }
         let transition = DaysElapse(29)
         let expectedState = Ineligible {DaysUntilEligible = 1; LastPrice = 100M }
         Assert.Equal(expectedState, currentState |> applyTransition(transition))
        
     [<Fact>]
-    let ``Start State, price decrease, restarts countdown with new price`` () =
+    let ``Price decrease restarts eligibility countdown with new price`` () =
         let currentState = Ineligible {DaysUntilEligible = 1; LastPrice = 100M }
         let transition = PriceDecrease(10M)
         let expectedState = Ineligible {DaysUntilEligible = 30; LastPrice = 90M }
         Assert.Equal(expectedState, currentState |> applyTransition(transition))
           
     [<Fact>]
-    let ``Start State, price increase, restarts countdown with new price`` () =
+    let ``Price increase restarts eligibility countdown with new price`` () =
         let currentState = Ineligible {DaysUntilEligible = 1; LastPrice = 100M }
         let transition = PriceIncrease(10M)
         let expectedState = Ineligible {DaysUntilEligible = 30; LastPrice = 110M }
         Assert.Equal(expectedState, currentState |> applyTransition(transition))
           
     [<Fact>]
-    let ``Start State, 30 days pass, transition to Eligible`` () =
+    let ``30 days with stable price results in eligibility`` () =
         let currentState = Ineligible {DaysUntilEligible = 1; LastPrice = 100M }
         let transition = DaysElapse(30)
         let expectedState = Eligible {Lower = 70M; Upper = 95M }
